851e05c635286b05e70a693bfd827dc2b4311a73
ThitiSuppradit <thiti-tc16@outlook.com>
2017-05-06 22:59:46 +0700

    Create Lab5 Potentiometer .ino

---
+++ Lab5 Potentiometer .ino
@@ -0,0 +1,15 @@
+int potPin = A0;   
+int ledPin = A1;   
+int val = 0;    
+
+void setup() {
+  pinMode(ledPin, OUTPUT);  
+}
+
+void loop() {
+  val = analogRead(potPin);    
+  digitalWrite(ledPin, HIGH); 
+  delay(val);                  
+  digitalWrite(ledPin, LOW);   
+  delay(val);       
+}
